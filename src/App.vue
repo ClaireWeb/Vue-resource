1d12b95ba9803852783db761581d2d5048a17dbe
Claire <clairepelletier.prod@gmail.com>
2020-01-24 13:26:42 +0100

    DONE

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="ui container">
+    <div class="ui active dimmer" v-if="loading">
+      <div class="ui loader"></div>
+    </div>
+    <div class="ui four column grid">
+      <div class="column" v-for="user in users" :key="user.id">
+        <div class="ui card">
+          <div class="content">
+            <div class="header">
+              <input type="text" v-model="user.name" style="width:100%" />
+            </div>
+            <div class="meta">
+              <a href="#">Friends</a>
+            </div>
+            <div class="description">
+              {{ user.address.street }} <br />
+              {{ user.address.zipcode }} {{ user.address.city }} <br />
+            </div>
+          </div>
+          <div class="extra content">
+            <div class="ui two buttons">
+              <button class="ui basic green button" @click="save(user)">
+                <i class="edit icon"></i>
+              </button>
+              <button class="ui basic red button" @click="destroy(user)">
+                <i class="trash icon"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      users: [],
+      loading: false
+    }
+  },
+  components: {},
+  methods: {
+    save(user) {
+      this.$user
+        .update(
+          { id: user.id },
+          {
+            name: user.name
+          }
+        )
+        .then(
+          res => {
+            console.log(res.data.name)
+          },
+          res => {
+            console.log('error', res)
+          }
+        )
+    },
+    destroy(user) {
+      this.$user.remove({ id: user.id }).then(
+        res => {
+          this.users = this.users.filter(u => u !== user)
+          console.log('delete success', res)
+        },
+        res => {
+          console.log('error', res)
+        }
+      )
+    }
+  },
+  mounted() {
+    // Définit toutes les requêtes, et mets en place un système de loader à chaque requête
+    this.$user = this.$resource(
+      'users{/id}',
+      {},
+      {},
+      {
+        before: () => {
+          this.loading = true
+        },
+        after: () => {
+          this.loading = false
+        }
+      }
+    )
+
+    this.$user.query().then(
+      res => {
+        this.users = res.data
+        // Si on a un doute sur le format renvoyé, permet de récupérer du json:
+        // res.json().then(data => {
+        //   this.users = data
+        // })
+      },
+      res => {
+        console.log('error', res)
+      }
+    )
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 
+Vue.use(VueResource)
+// Configure l'url de toutes les requêtes
+Vue.http.options.root = 'https://jsonplaceholder.typicode.com'
+// Pour ajouter différents header, ici web token
+Vue.http.headers.common['Authorization'] = 'Basic YXBponBhc'
+
+// Création de la méthode after de l'intercepteur
+Vue.http.interceptors.push((request, next) => {
+  next(res => {
+    if (request.after) {
+      request.after.call(this, res)
+    }
+  })
+})
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
